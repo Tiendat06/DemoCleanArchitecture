266a7e6d7286372a6a58f398c59b5025958c3629
Tiendat06 <tiendat79197@gmail.com>
2026-06-21 15:26:10 +0700

    Restrict Swagger UI to Development env
    Commented out the `app.Environment.EnvironmentName == "InternalDevelopment"` check in Program.cs so Swagger and SwaggerUI are only enabled when `app.Environment.IsDevelopment()` is true. This limits the Swagger UI to the standard Development environment during the HTTP pipeline configuration.

--- Demo.API/Program.cs
+++ Demo.API/Program.cs
@@ -85,7 +85,7 @@ builder.Services.AddAuthentication(x =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment() || app.Environment.EnvironmentName == "InternalDevelopment")
+if (app.Environment.IsDevelopment() /*|| app.Environment.EnvironmentName == "InternalDevelopment"*/)
 {
     app.UseSwagger();
     app.UseSwaggerUI();
